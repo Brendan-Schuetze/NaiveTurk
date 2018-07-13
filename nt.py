3b538dc56096bebeb47c09abd541ce6131638e6d
Brendan Schuetze <Brendan.A.Schuetze.18@dartmouth.edu>
2018-07-13 15:42:42 -0400

    Add list converter
    Beginning support for tags

--- nt.py
+++ nt.py
@@ -1,25 +1,42 @@
+# Import Libraries
 from flask import Flask
 from flask_pymongo import PyMongo
 from time import gmtime, strftime
+from werkzeug.routing import BaseConverter
 
+# Start Flask App
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/turk"
 mongo = PyMongo(app)
 
-@app.route("/r/<user>", methods = ['GET'])
-def returnUserStatus(user): 
+# List Converter for Converting Tags to a List
+class ListConverter(BaseConverter):
+
+    def to_python(self, value):
+        return value.split('+')
+
+    def to_url(self, values):
+        return '+'.join(BaseConverter.to_url(value)
+                        for value in values)
+
+app.url_map.converters['list'] = ListConverter
+
+
+# Method for Checking if User is in Database
+@app.route("/check/<user>/<list:tags>", methods = ['GET'])
+def returnUserStatus(user):
     user_doc = mongo.db.id.find_one({"worker": user})
- 
+
     if user_doc is None:
         return("User not found")
-    else: 
+    else:
         id = user_doc["_id"]
-        mongo.db.id.update({ "_id" : id}, { "$push": { "pings": strftime("%Y-%m-%d %H:%M:%S", gmtime()) }})        
-        
-        return(str(user_doc["worker"]) + "," +  str(len(list(user_doc["pings"]))))   
+        mongo.db.id.update({ "_id" : id}, { "$push": { "pings": strftime("%Y-%m-%d %H:%M:%S", gmtime()) }})
 
+        return(str(user_doc["worker"]) + "," +  str(len(list(user_doc["pings"]))) + " " + str(tags))
 
-@app.route("/r/<user>", methods = ['POST'])
+# Method for Updating Tags Associated with User
+@app.route("/update/<user>", methods = ['POST'])
 def updateUserStatus(user):
     return "Testing"
 
@@ -29,7 +46,6 @@ def updateUserStatus(user):
 @app.route("/")
 def csv():
     return "Welcome to NaiveTurk"
- 
+
 if __name__ == "__main__":
     app.run(host ='0.0.0.0', debug = True)
-

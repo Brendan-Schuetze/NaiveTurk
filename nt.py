89f0d2235aa263d339b937552b5f10df1543b880
Brendan Schuetyze <brendan.schuetze@gmail.com>
2018-07-13 19:36:56 +0000

    Adding Database Ops

--- nt.py
+++ nt.py
@@ -1,17 +1,27 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from time import gmtime, strftime
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/turk"
 mongo = PyMongo(app)
 
 @app.route("/r/<user>", methods = ['GET'])
-def returnUserStatus(user):
-    
+def returnUserStatus(user): 
+    user_doc = mongo.db.id.find_one({"worker": user})
+ 
+    if user_doc is None:
+        return("User not found")
+    else: 
+        id = user_doc["_id"]
+        mongo.db.id.update({ "_id" : id}, { "$push": { "pings": strftime("%Y-%m-%d %H:%M:%S", gmtime()) }})        
+        
+        return(str(user_doc["worker"]) + "," +  str(len(list(user_doc["pings"]))))   
 
 
 @app.route("/r/<user>", methods = ['POST'])
 def updateUserStatus(user):
+    return "Testing"
 
 
 
@@ -21,5 +31,5 @@ def csv():
     return "Welcome to NaiveTurk"
  
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0')
+    app.run(host ='0.0.0.0', debug = True)
 
